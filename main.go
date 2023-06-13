0c30f7515387b53341c1edb659bff1b1ff4ea0a1
caden32 <caden@caden32.com>
2023-06-13 13:56:41 -0600

    add more functionality

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"math"
 	"math/rand"
 	"time"
 )
@@ -15,20 +15,21 @@ const (
 )
 
 func main() {
-	t := time.NewTicker(time.Millisecond * time.Duration(rand.Intn(999)))
-	<-t.C
-	fmt.Println("ticked")
+	_ = NewServer(true)
+
 }
 
 type Server struct {
+	BootStrap   bool
 	Id          int
 	CurrentTerm int
 	VotedFor    int
-	Log         []int
+	Log         []LogEntry
 	CommitIndex int
 	LastApplied int
 	NextIndex   []int
 	MatchIndex  []int
+	Timeout     *time.Ticker
 	State       ServerState
 }
 
@@ -36,4 +37,58 @@ type LogEntry struct {
 	Term  int
 	Index int
 	Data  int
+}
+
+type RequestVote struct {
+	Term         int
+	CandidateId  int
+	LastLogIndex int
+	LastLogTerm  int
+}
+
+type AppendLog struct {
+	Term         int
+	LeaderId     int
+	PrevLogIndex int
+	PrevLogTerm  int
+	Entries      LogEntry
+	LeaderCommit int
+}
+
+type Response struct {
+	Term        int
+	VoteGranted bool
+}
+
+func NewServer(BootStrap bool) Server {
+	rn := rand.Intn(math.MaxInt)
+	for rn == 0 {
+		rn = rand.Intn(math.MaxInt)
+	}
+	return Server{
+		BootStrap:   BootStrap,
+		Id:          rn,
+		CurrentTerm: 0,
+		VotedFor:    0,
+		Log:         []LogEntry{},
+		CommitIndex: 0,
+		LastApplied: 0,
+		NextIndex:   []int{},
+		MatchIndex:  []int{},
+		Timeout:     time.NewTicker(time.Millisecond * time.Duration(rand.Intn(999))),
+		State:       Follower,
+	}
+}
+
+func (s *Server) ReceiveVote(request RequestVote) Response {
+	if request.Term < s.CurrentTerm {
+		return Response{
+			s.CurrentTerm,
+			false,
+		}
+	} // else if s.VotedFor != 0 ||
+	return Response{
+		s.CurrentTerm,
+		false,
+	}
 }
